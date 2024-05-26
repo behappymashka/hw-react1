95b84f343b1ab8eca23357eb82b01f2abc937043
behappymashka <98Mashenka@gmail.com>
2024-05-26 17:54:24 +0300

    any

---
+++ src/components/MainContainer/MainContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainContainer.css';
+
+function MainContainer() {
+    return (
+        <main className="main-container">
+            <h2>Сentral Container</h2>
+            <p>This is the central container</p>
+        </main>
+    );
+}
+
+export default MainContainer;
---
+++ src/components/Sider/Sider.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Sider.css';
+
+function Sidebar() {
+    return (
+        <aside className="sidebar">
+            <ul>
+                <li>Navigation Item 1</li>
+                <li>Navigation Item 2</li>
+                <li>Navigation Item 3</li>
+            </ul>
+        </aside>
+    );
+}
+
+export default Sidebar;
